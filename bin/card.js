21d32e8fedf719601f63a55b6daed022d0ffa991
jcha0713 <joocha0713@gmail.com>
2022-09-03 23:58:16 -0700

    Add cli tool

--- bin/card.js
+++ bin/card.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
 
+import * as url from "url";
+import fs from "fs";
+import path from "path";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const card = fs.readFileSync(path.join(__dirname, "card"), "utf8");
+console.log(card);
